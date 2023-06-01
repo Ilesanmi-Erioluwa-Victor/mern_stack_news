84e17cd8ddd25792d1c98ecebfc8f3c05903b975
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-01 20:49:04 +0100

    Working on Nav type

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
-
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { HomeIcon } from '@heroicons/react/24/solid';
 type Nav = {
-    name: string,
-    title: string,
-    icon: string,
-    id: number,
-    link : string
-}
+  name: string;
+  title: string;
+  icon: object;
+  id: number;
+  link: string;
+};
+
+const NavLink: Nav[] = [
+  {
+    name: 'Home',
+    title: 'home',
+    icon: <HomeIcon />,
+    id: uuidv4(),
+    link: '/',
+  },
+];
 
 const Nav = () => {
   return (
     <div>
-      
+      {NavLink.map((nav: any) => {
+        return (
+          <ul key={nav.id}>
+            <li>{nav.name}</li>
+          </ul>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Nav
-
+export default Nav;
