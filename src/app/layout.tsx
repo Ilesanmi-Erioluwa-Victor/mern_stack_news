23fac975fd069321e873111737b7102c78c3afa6
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-02 18:04:53 +0100

    Installed Nprogress package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
-
+import NProgress from "nprogress"
 import './globals.css';
 import Nav from 'src/components/Navigation/Nav';
 
@@ -18,13 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const head = () => (
-    //  <!-- Latest compiled and minified CSS -->
+    <>
+     {/* //  <!-- Latest compiled and minified CSS --> */}
     <link
       rel='stylesheet'
       href='https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css'
       integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u'
       crossOrigin='anonymous'
-    />
+      />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+      </>
   );
 
   return (
