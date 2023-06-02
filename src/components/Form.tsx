e1dfafa3cfa0a841a72006ecbb6aca27bda05a75
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-02 23:58:56 +0100

    Working on Register page

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React from 'react';
 
-const Form = ():JSX.Element => {
+const Form = (): JSX.Element => {
   return (
-    <form >
-      
+    <form>
+      <fieldset className='form-group'>
+        <label
+          htmlFor='name'
+          className='text-lg'
+        >
+          Name
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter your Name'
+        />
+      </fieldset>
+
+      <fieldset className='form-group'>
+        <label
+          htmlFor='email'
+          className='text-lg'
+        >
+          Email
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter your Email'
+        />
+      </fieldset>
+
+      <fieldset className='form-group'>
+        <label
+          htmlFor='password'
+          className='text-lg'
+        >
+          Password
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter your password '
+        />
+      </fieldset>
+
+      <fieldset className='form-group'>
+        <button className="btn btn-outline-warning text-dark border border-blue-400 :hover-text-white">Register</button>
+      </fieldset>
     </form>
-  )
-}
+  );
+};
 
-export default Form
-
+export default Form;
