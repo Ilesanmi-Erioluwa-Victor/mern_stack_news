50ef068e28b29647ea62017492d888369d76a084
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-03 18:23:16 +0100

    bug fixed on  on register router

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('api/register', (req, res) => {
+app.get('/api/register', (req, res) => {
   res.json({
     data: 'You hit register endpoint',
   });
