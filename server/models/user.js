208430501dfb2c6ba8b16c43ed403ce59eba0eb2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-03 20:08:44 +0100

    Working on userScema in Models folder

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,11 @@ const userSchema = new mongoose.Shema({
     username : {
         type : String,
         trim : true,
-        required : true
-    }
+        required: true,
+        max: 30,
+        unique: true,
+        index: true,
+        lowercase : true
+    },
+    
 })
