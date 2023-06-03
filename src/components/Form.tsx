56bc61b412665e351fcf2fbb5c79a9254a7df3bc
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-03 01:08:10 +0100

    Restructured my handleInput function

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -26,6 +26,7 @@ const Form = (): JSX.Element => {
   //     setData({ ...data, [name]: value.trim() });
   //   };
 
+    const {name, password, email, success, error, buttonText } = data;
   const handleInputChange = (name: string) => (ev: any) => {
     setData({ ...data, [name]: ev.target.value.trim(), error : "", success : "", buttonText : "Register" });
   };
@@ -38,64 +39,68 @@ const Form = (): JSX.Element => {
         password: ''
     });
   };
-  return (
-    <form onSubmit={handleSubmit}>
-      <fieldset className='form-group'>
-        <label
-          htmlFor='name'
-          className='fs-4 pb-2'
-        >
-          Name
-        </label>
-        <input
-          type='text'
-          className='form-control'
-          placeholder='Enter your Name'
-          onChange={handleInputChange('name')}
-          //   name='name'
-          //   value={data.name}
-        />
-      </fieldset>
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <fieldset className='form-group'>
+            <label
+              htmlFor='name'
+              className='fs-4 pb-2'
+            >
+              Name
+            </label>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Enter your Name'
+              onChange={handleInputChange('name')}
+              value={name}
+            />
+          </fieldset>
 
-      <fieldset className='form-group'>
-        <label
-          htmlFor='email'
-          className='fs-4 pb-2'
-        >
-          Email
-        </label>
-        <input
-          type='email'
-          className='form-control'
-          placeholder='Enter your Email'
-          onChange={handleInputChange('email')}
-          //   name='email'
-          //   value={data.email}
-        />
-      </fieldset>
+          <fieldset className='form-group'>
+            <label
+              htmlFor='email'
+              className='fs-4 pb-2'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              className='form-control'
+              placeholder='Enter your Email'
+              onChange={handleInputChange('email')}
+              value={email}
+            />
+          </fieldset>
 
-      <fieldset className='form-group'>
-        <label
-          htmlFor='password'
-          className='fs-4 pb-2'
-        >
-          Password
-        </label>
-        <input
-          type='password'
-          className='form-control'
-          placeholder='Enter your password '
-          //   name='password'
-          //   value={data.password}
-          onChange={handleInputChange('password')}
-        />
-      </fieldset>
+          <fieldset className='form-group'>
+            <label
+              htmlFor='password'
+              className='fs-4 pb-2'
+            >
+              Password
+            </label>
+            <input
+              type='password'
+              className='form-control'
+              placeholder='Enter your password '
+              value={password}
+              onChange={handleInputChange('password')}
+            />
+          </fieldset>
 
-      <div className='form-group'>
-        <button className='btn btn-outline-warning fs-4'>Register</button>
-      </div>
-    </form>
-  );
+          <div className='form-group'>
+            <button className='btn btn-outline-warning fs-4'>
+              {buttonText}
+            </button>
+          </div>
+            </form>
+            <div>
+                {JSON.stringify(data)}
+            </div>
+      </>
+    );
 };
 
 export default Form;
