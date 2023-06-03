5ef0c99f07526a0c29424f4561c27fe0da8fd40d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-03 19:40:13 +0100

    Working on auth.js file in routes folder

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const app = express();
+const authRoutes = require('./routes/auth');
 
-
+// middleware
+app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () =>  console.log(`You are successfully connected to : ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`You are successfully connected to : ${PORT}`)
+);
