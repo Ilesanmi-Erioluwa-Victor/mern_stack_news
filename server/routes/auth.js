56df0106e4451c8b0c246893349e814797d1faa8
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-03 19:35:29 +0100

    Working on auth.js file in routes folder

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,12 @@
-const express = require("express")
+const express = require('express');
 
 const router = express.Router();
 
 router.get('/api/register', (req, res) => {
   res.json({
-      data: 'You hit register endpoint',
-      status : "Success"
+    data: 'You hit register endpoint',
+    status: 'Success',
   });
-})
+});
+
+module.exports = router;
