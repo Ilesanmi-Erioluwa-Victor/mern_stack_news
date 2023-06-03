5c62ae18f144cf5899be25092198272058ff4135
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-03 20:40:15 +0100

    Working on userScema in Models folder

--- server/models/user.js
+++ server/models/user.js
@@ -1,47 +1,58 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Shema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    max: 30,
-    unique: true,
-    index: true,
-    lowercase: true,
-  },
+const userSchema = new mongoose.Shema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 30,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
 
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    max: 40,
-  },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 40,
+    },
 
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
 
-  hashed_password: {
-    type: String,
-    required: true,
-  },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
 
-  salt: String,
+    salt: String,
 
     role: {
-        type: String,
-        default : "subscriber"
+      type: String,
+      default: 'subscriber',
     },
 
     resetPasswordLink: {
-        data: String,
-        default : ""
-    }
-}, {
-    timestamps : true
-});
+      data: String,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Virtual fields
+userSchema.virtual();
+
+// methods > authenticate method, encrypt password method, makeSalt value method
+userSchema.methods = {
+
+}
