13d2ab010bff6f0baff3175add432ef526a99780
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-04 21:58:36 +0100

    Working on Validators aspects of code

--- server/validators/authVal.js
+++ server/validators/authVal.js
@@ -3,5 +3,5 @@ const { check } = require("express-validator")
 exports.userRegisterValidator = [
   check('name').not().isEmpty().withMessage('Please, Name is required...'),
   check('email').isEmail().withMessage('Must be a valid email address..'),
-  check('password').isLength({min : 6}).withMessage('Password  must be greater than 6 characters long...'),
+  check('password').isLength({min : 4}).withMessage('Password  must be greater than 4 characters long...'),
 ];
