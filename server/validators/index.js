225f9d3bbd0375716572eb9ad43dacc91647b58f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-04 21:41:09 +0100

    Working on Validators aspects of code

--- server/validators/index.js
+++ server/validators/index.js
@@ -1,5 +1,9 @@
 const { validationResult } = require('express-validator');
 
 exports.runValidation = (req, res, next) => {
-    const errors = validationResult(req)
-}
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return
+    }
+};
