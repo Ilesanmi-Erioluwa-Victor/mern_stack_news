90ed0ef39ec1591b06dfe9c0d0cc774b3b45b9b9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-04 21:48:05 +0100

    Working on Validators aspects of code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { registerCtrl } = require('../controllers/authCtrl');
+
+// Validations <>Middleware<>
 const { userRegisterValidator } = require('../validators/authVal');
 const { runValidation } = require('../validators');
-runValidation
 
-router.post('/register', registerCtrl);
+
+router.post('/register',userRegisterValidator, runValidation, registerCtrl);
 
 module.exports = router;
