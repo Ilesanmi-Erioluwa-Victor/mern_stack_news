6e5fc30d4aa64bd003734a006ed85075aa739f2a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-04 01:33:24 +0100

    Setting up my redux toolkit work flow

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,18 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 import { userApi } from './services/userApi';
-setupListeners
+
 export const store = configureStore({
   reducer: {
-    [userApi.reducerPath] : userApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
-  middleware: (getDefaultMiddleware) => (
-    
-  )
-    
-  ,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([userApi.middleware]),
 });
 
+setupListeners(store.dispatch);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
