b2248005b59725d380819496b0e14891a4ab1ef1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-05 21:38:51 +0100

    Working on aws sdk

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -1,11 +1,20 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-  accessKeyId: "",
-  secretAccessKey: "",
-  region  :""
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: '',
+});
+
+const ses = AWS.SES({
+  apiVersion: '2010-12-01',
 });
 
 exports.registerCtrl = (req, res) => {
+  const { name, email, password } = req?.body;
+
+  const params = {
+    
+  }
   console.log('Well conneted ', req?.body);
 };
