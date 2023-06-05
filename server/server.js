5c76b63e74a1b492c9693f37370380a8ddd7c8e0
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-06-05 18:18:47 +0100

    Connected my db successfully

--- server/server.js
+++ server/server.js
@@ -9,12 +9,14 @@ const app = express();
 const authRoutes = require('./routes/auth');
 
 // DB connect
-mongoose.connect(process.env.DATABAS_CLOUD)
+mongoose
+  .connect(process.env.DATABAS_CLOUD)
+  .then(() => console.log('DB Successfully connected..'))
+  .catch( (err) => console.log(err));
 // app middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
 
 // middleware
 app.use(express.json());
